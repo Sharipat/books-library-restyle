91b12a278f5f0670fc3d9dca07cba9d4884a9f65
Shirlusha <sheerly.morra@gmail.com>
2021-08-16 18:55:47 +0300

    Add list comprehension
    - Delete print dictionary
    - Fix README.md

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 import os
 from urllib.parse import urljoin
 
@@ -11,18 +12,13 @@ from tqdm import trange
 def check_for_redirect(response):
     if response.history:
         raise requests.HTTPError
-    return response
 
 
-def parse_book_name(response):
-    soup = BeautifulSoup(response.text, 'lxml')
+def parse_book_name(soup):
     title_soup = soup.find('div', id='content')
-    if title_soup:
-        title_text = title_soup.find('h1').text.strip().split(' :: ')
-        title = sanitize_filename(title_text[0].strip())
-        author = sanitize_filename(title_text[1].strip())
-        title_tuple = (title, author,)
-        return title_tuple
+    title_text = title_soup.find('h1').text.strip().split(' :: ')
+    title, author = title_text
+    return title.strip(), author.strip()
 
 
 def download_txt(url, title, number, folder):
@@ -32,34 +28,20 @@ def download_txt(url, title, number, folder):
     book_url = urljoin(url, 'txt.php?id={}'.format(number))
     text_response = requests.get(book_url)
     text_response.raise_for_status()
-    try:
-        with open(book, 'wb') as file:
-            file.write(text_response.content)
-    except FileExistsError:
-        return
+    with open(book, 'w+') as file:
+        file.write(text_response.text)
 
 
-def download_comments(response, number, folder):
-    folder = os.path.join(folder)
-    os.makedirs(folder, exist_ok=True)
-    soup = BeautifulSoup(response.text, 'lxml')
-    comments = soup.find_all('div', class_='texts')
+def parse_comments(soup):
+    comments = soup.select('div.texts span')
     if comments:
-        comments_path = os.path.join(folder, f'{number}.txt')
-        with open(comments_path, 'w+') as comment_file:
-            for comment in comments:
-                comment_text = comment.find('span', class_='black').text
-                comment_file.write(comment_text + '\n')
+        comment_text = [comment for comment in comments]
         return comment_text
 
 
-def parse_book_genre(response):
-    soup = BeautifulSoup(response.text, 'lxml')
-    genres_soup = soup.find_all('div', id='content')
-    genres = []
-    for genre in genres_soup:
-        genre = genre.find('span', class_='d_book').text.strip().split(': \xa0')
-        genres.append(sanitize_filename(genre[1]))
+def parse_book_genre(soup):
+    genres_soup = soup.select('span.d_book a')
+    genres = [genre for genre in genres_soup]
     return genres
 
 
@@ -68,24 +50,15 @@ def parse_book_image(response):
     return soup.select_one('div.bookimage img')['src']
 
 
-def download_book_image(image_soup, url, folder):
+def download_book_image(image_src, url, folder):
     folder = os.path.join(folder)
     os.makedirs(folder, exist_ok=True)
-    image_url = urljoin(url, image_soup)
+    image_url = urljoin(url, image_src)
     image_response = requests.get(image_url)
     image_response.raise_for_status()
-    image = os.path.join(folder, sanitize_filename(image_soup))
+    image = os.path.join(folder, sanitize_filename(image_src))
     with open(image, 'wb') as image_file:
         image_file.write(image_response.content)
-
-
-def parse_book_page(title, author, genre, comments):
-    page_info = {'Автор: ': author,
-                 'Заголовок: ': title,
-                 'Жанр: ': genre,
-                 'Комментарии: ': comments
-                 }
-    return page_info
 
 
 def parse_book_args():
@@ -107,25 +80,29 @@ def parse_book_args():
 
 def main():
     args = parse_book_args()
-    url = 'https://tululu.org/'
-    for number in trange(args.start_id, (args.end_id +1)):
-        name_url = urljoin(url, 'b{}/'.format(number))
+    base_url = 'https://tululu.org/'
+    for number in trange(args.start_id, (args.end_id + 1)):
+        url = urljoin(base_url, 'b{}/'.format(number))
         try:
-            response = requests.get(name_url)
+            response = requests.get(url)
             response.raise_for_status()
-            if not response.history:
-                title, author = parse_book_name(response)
-                parse_book_genre(response)
-                download_txt(url, title, number, folder='books/')
-                genre = parse_book_genre(response)
-                comments = download_comments(response, number, folder='comments/')
-                page_info = parse_book_page(title, author, genre, comments)
-                print(page_info)
-                image_soup = parse_book_image(response)
-                if 'nopic.gif' not in sanitize_filename(image_soup):
-                    download_book_image(image_soup, url, folder='covers/')
+            check_for_redirect(response)
+            soup = BeautifulSoup(response.text, 'lxml')
+            title, author = parse_book_name(soup)
+            download_txt(base_url, title, number, folder='books/')
+            genre = parse_book_genre(soup)
+            comments = parse_comments(soup)
+            page_info = {'Автор: ': author,
+                         'Заголовок: ': title,
+                         'Жанр: ': genre,
+                         'Комментарии: ': comments
+                         }
+            logging.info(page_info)
+            image_src = parse_book_image(response)
+            if 'nopic.gif' not in sanitize_filename(image_src):
+                download_book_image(image_src, base_url, folder='covers/')
         except requests.HTTPError:
-            print(f'No page at {name_url}')
+            print(f'No page at {url}')
 
 
 if __name__ == '__main__':
