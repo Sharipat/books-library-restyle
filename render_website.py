c54e06c36facc687ad23b34682218bfe38b2da48
Shirlusha <sheerly.morra@gmail.com>
2021-09-01 20:48:17 +0300

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -1,12 +1,11 @@
 import collections
 import os
 
+from more_itertools import chunked
 import pandas
 from dotenv import load_dotenv
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
-
-
 
 
 def get_books(json_path):
@@ -17,11 +16,16 @@ def get_books(json_path):
     return sorted(books.items())
 
 
-def render_page(env, books):
+def render_page(env, books, dest_folder, pages_folder):
+    folder = os.path.join(dest_folder, pages_folder)
+    os.makedirs(folder, exist_ok=True)
     template = env.get_template('template.html')
-    rendered_page = template.render(books_catalog=books)
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    chunked_books = chunked(books, 10)
+    for number, page in enumerate(chunked_books, 1):
+        page_path = os.path.join(folder, f'index{number}.html')
+        rendered_page = template.render(books_catalog=page)
+        with open(page_path, 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 
 def main():
@@ -30,8 +34,10 @@ def main():
         loader=FileSystemLoader('templates'),
         autoescape=select_autoescape(['html', 'xml']))
     json_path = os.getenv('JSON_PATH')
+    dest_folder = os.getenv('DEST_FOLDER')
+    pages_folder = os.getenv('PAGES_FOLDER')
     books = get_books(json_path)
-    render_page(env, books)
+    render_page(env, books, dest_folder, pages_folder)
     server = Server()
     server.watch('template.html', render_page)
     server.serve(root='.')
