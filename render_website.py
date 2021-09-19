91ce6c542aa7f3c9c2198d84230cf77baad2ccbd
Shirlusha <sheerly.morra@gmail.com>
2021-09-19 21:51:23 +0300

    Add indicator for current page

--- render_website.py
+++ render_website.py
@@ -20,30 +20,7 @@ def get_books(json_path):
     return sorted(books.items())
 
 
-def render_page(env, books, dest_folder, pages_folder, static_url):
-    folder = os.path.join(dest_folder, pages_folder)
-    os.makedirs(folder, exist_ok=True)
-    template = env.get_template('template.html')
-    chunked_books = list(chunked(books, BOOKS_ON_PAGE))
-    pages_number = math.ceil(len(chunked_books))
-    covers_path = urljoin(dest_folder, 'covers/nopic.gif')
-    covers_url = unquote(covers_path)
-    for number, page in enumerate(chunked_books, 1):
-        page_path = os.path.join(folder, f'index{number}.html')
-        previous_page = os.path.join(f'index{number - 1}.html') if number - 1 > 0 else None
-        next_page = os.path.join(f'index{number + 1}.html') if number + 1 <= pages_number else None
-        all_pages = [
-            {'number': number, 'url': os.path.join(f'index{number}.html')
-             } for number in range(1, pages_number + 1)]
-        rendered_page = template.render(static_url=static_url, covers_url=covers_url,
-                                        books_catalog=page, next_page=next_page, previous_page=previous_page,
-                                        all_pages=all_pages)
-        with open(page_path, 'w', encoding="utf8") as page_file:
-            page_file.write(rendered_page)
-
-
-def main():
-    load_dotenv()
+def on_reload():
     env = Environment(
         loader=FileSystemLoader('templates'),
         autoescape=select_autoescape(['html', 'xml']))
@@ -52,10 +29,34 @@ def main():
     pages_folder = os.getenv('PAGES_FOLDER', 'pages')
     static_url = os.getenv('STATIC_URL')
     books = get_books(json_path)
-    render_page(env, books, dest_folder, pages_folder, static_url)
+    folder = os.path.join(dest_folder, pages_folder)
+    os.makedirs(folder, exist_ok=True)
+    template = env.get_template('template.html')
+    chunked_books = list(chunked(books, BOOKS_ON_PAGE))
+    pages_number = math.ceil(len(chunked_books))
+    covers_path = os.path.join(dest_folder, 'covers/nopic.gif')
+    covers_url = unquote(covers_path)
+    for page_number, page in enumerate(chunked_books, 1):
+        print(page_number)
+        page_path = os.path.join(folder, f'index{page_number}.html')
+        previous_page = os.path.join(f'index{page_number - 1}.html') if page_number - 1 > 0 else None
+        next_page = os.path.join(f'index{page_number + 1}.html') if page_number + 1 <= pages_number else None
+        all_pages = [
+            {'number': number, 'url': os.path.join(f'index{number}.html')
+             } for number in range(1, pages_number + 1)]
+        rendered_page = template.render(static_url=static_url, covers_url=covers_url, current_page_number=page_number,
+                                        books_catalog=page, next_page=next_page, previous_page=previous_page,
+                                        all_pages=all_pages)
+        with open(page_path, 'w', encoding="utf8") as page_file:
+            page_file.write(rendered_page)
+
+
+def main():
+    load_dotenv()
+    on_reload()
     server = Server()
-    server.watch('template.html', render_page)
-    server.serve(root='.')
+    server.watch('templates/template.html', on_reload)
+    server.serve(port=8000)
 
 
 if __name__ == '__main__':
